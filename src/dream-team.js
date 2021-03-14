38d12ece600e61f6cf418d756ae8e433b4f2ce31
Elena Surilova <elen21surilova@gmail.com>
2021-03-15 00:23:32 +0200

    feat: add solution for task Dream Team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	if (!Array.isArray(members)) {
+		return false;
+	}
+	const newArr = [];
+	for (let i = 0; i < members.length; i++) {
+		if (typeof members[i] === 'string') {
+			let name = members[i].trim();
+			name = name.toUpperCase();
+			newArr.push(name[0]);
+		}
+	}
+
+	function findFirstLetter(arr) {
+		let first = arr[0];
+		let firstIndex = 0;
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] < first) {
+				first = arr[i];
+				firstIndex = i;
+			}
+		}
+		return firstIndex;
+	}
+
+	const resultArr = [];
+	const arrLength = newArr.length;
+	for (let i = 0; i < arrLength; i++) {
+		let firstLetter = findFirstLetter(newArr);
+		resultArr.push(newArr[firstLetter]);
+		newArr.splice(firstLetter, 1);
+	}
+
+	const resultStr = resultArr.join('');
+	return resultStr;
 };
