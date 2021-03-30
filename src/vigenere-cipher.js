4c91bf7a9aa79c058c34e827b529e9f4aa91d7aa
Elena Surilova <elen21surilova@gmail.com>
2021-03-30 17:13:03 +0300

    feat: add full my solution for the task vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,36 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 const ALPHABET = {
-	0: 'a',
-	1: 'b',
-	2: 'c',
-	3: 'd',
-	4: 'e',
-	5: 'f',
-	6: 'g',
-	7: 'h',
-	8: 'i',
-	9: 'j',
-	10: 'k',
-	11: 'l',
-	12: 'm',
-	13: 'n',
-	14: 'o',
-	15: 'p',
-	16: 'q',
-	17: 'r',
-	18: 's',
-	19: 't',
-	20: 'u',
-	21: 'v',
-	22: 'w',
-	23: 'x',
-	24: 'y',
-	25: 'z'
+	0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j', 10: 'k', 11: 'l', 12: 'm', 13: 'n',
+	14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z'
 };
 
-const ALPHABET_LENGTH = 25; //так как в объекте начинаю с нуля, то 26 - 1
+const regexp = /\s*\W/g;
+
+const ALPHABET_LENGTH = 26;
 class VigenereCipheringMachine {
+	constructor(isDirect) {
+		this.isDirect = isDirect;
+	}
 	encrypt(message, key) {
 		this.message = message;
 		this.key = key;
@@ -42,56 +23,69 @@ class VigenereCipheringMachine {
 		const arrKey = [];
 		const arrMess = [];
 		const sum = [];
-		const resultArr = [];
+		let resultArr = [];
 		let resultStr = '';
 
-		for (const s of key) {
-			for (const k in ALPHABET) {
-				if (s === ALPHABET[k]) {
-					arrKey.push(k);
+		for (const symb of key.toLowerCase()) {
+			for (const keyAlphabet in ALPHABET) {
+				if (symb === ALPHABET[keyAlphabet]) {
+					arrKey.push(keyAlphabet);
 				}
 			}
 		}
-		for (const s of message) {
-			for (const k in ALPHABET) {
-				if (s === ALPHABET[k]) {
-					arrMess.push(k);
+
+		for (let symb of message.toLowerCase()) {
+			for (let letter in ALPHABET) {
+				if (symb === ALPHABET[letter]) {
+					letter = Number(letter);
+					arrMess.push(letter);
+					break;
+				}
+				if (regexp.test(symb) || /[0-9]+/.test(symb)) {
+					symb = String(symb);
+					arrMess.push(symb);
+					break;
 				}
 			}
 		}
 
 		for (let i = 0, j = 0; i < arrMess.length; i++, j++) {
-			sum.push(Number(arrMess[i]) + Number(arrKey[j]));
-
+			if (typeof arrMess[i] === 'string') {
+				sum.push(arrMess[i]);
+				j -= 1;
+			} else if (typeof arrMess[i] === 'number') {
+				sum.push(Number(arrMess[i]) + Number(arrKey[j]));
+			}
 			if (j === (arrKey.length - 1)) {
 				j = -1;
 			}
-
 		}
 
 		for (let i = 0; i < sum.length; i++) {
-			if (sum[i] > 25) {
-				resultArr.push(sum[i] - 26);
+			if (sum[i] >= ALPHABET_LENGTH) {
+				resultArr.push(sum[i] - ALPHABET_LENGTH);
 			} else {
 				resultArr.push(sum[i]);
 			}
 		}
 
-		for (let n = 0, startIndex = 0; n <= resultArr.length; n++) {
-			let spaceIndex = message.indexOf(' ', startIndex);
-			if (n === spaceIndex) {
-				resultStr += ' ';
-				startIndex = n-1;
-			}
-			resultStr += ALPHABET[resultArr[n]];
+		if (this.isDirect === false) {
+			resultArr = resultArr.reverse();
+		} else if (this.isDirect === true || this.isDirect === '') {
+			resultArr = resultArr;
+		}
 
-			if (n === resultArr.length - 1) {
-				resultStr = resultStr.toUpperCase();
-				return resultStr;
+		for (let i = 0; i < resultArr.length; i++) {
+			if (resultArr[i] in ALPHABET && typeof resultArr[i] === 'number') {
+				resultStr += ALPHABET[resultArr[i]];
+			} else {
+				resultStr += resultArr[i];
 			}
 		}
-	}
 
+		resultStr = resultStr.toUpperCase();
+		return resultStr;
+	}
 
 
 	decrypt(message, key) {
@@ -101,6 +95,73 @@ class VigenereCipheringMachine {
 		if (message === undefined || key === undefined) {
 			throw new Error('не переданы нужные аргументы');
 		}
+
+		const arrKey = [];
+		const arrMess = [];
+		const sum = [];
+		let resultArr = [];
+		let resultStr = '';
+
+		for (const symb of key.toLowerCase()) {
+			for (const keyAlphabet in ALPHABET) {
+				if (symb === ALPHABET[keyAlphabet]) {
+					arrKey.push(keyAlphabet);
+				}
+			}
+		}
+
+		for (let symb of message.toLowerCase()) {
+			for (let letter in ALPHABET) {
+				if (symb === ALPHABET[letter]) {
+					letter = Number(letter);
+					arrMess.push(letter);
+					break;
+				}
+				if (regexp.test(symb) || /[0-9]+/.test(symb)) {
+					symb = String(symb);
+					arrMess.push(symb);
+					break;
+				}
+			}
+		}
+
+			for (let i = 0, j = 0; i < arrMess.length; i++, j++) {
+			if (typeof arrMess[i] === 'string') {
+				sum.push(arrMess[i]);
+				j -= 1;
+			} else if (typeof arrMess[i] === 'number') {
+				sum.push(Number(arrMess[i]) + ALPHABET_LENGTH - Number(arrKey[j]));
+			}
+			if (j === (arrKey.length - 1)) {
+				j = -1;
+			}
+		}
+
+
+		for (let i = 0; i < sum.length; i++) {
+			if (sum[i] >= ALPHABET_LENGTH) {
+				resultArr.push(sum[i] - ALPHABET_LENGTH);
+			} else {
+				resultArr.push(sum[i]);
+			}
+		}
+
+		if (this.isDirect === false) {
+			resultArr = resultArr.reverse();
+		} else if (this.isDirect === true || this.isDirect === '') {
+			resultArr = resultArr;
+		}
+
+		for (let i = 0; i < resultArr.length; i++) {
+			if (resultArr[i] in ALPHABET && typeof resultArr[i] === 'number') {
+				resultStr += ALPHABET[resultArr[i]];
+			} else {
+				resultStr += resultArr[i];
+			}
+		}
+
+		resultStr = resultStr.toUpperCase();
+		return resultStr;
 	}
 }
 
