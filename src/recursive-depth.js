1799aefd2f14edab5443e9605b29e68944f59410
helen25sur <65970892+helen25sur@users.noreply.github.com>
2022-05-01 23:40:24 +0300

    feat: add solution for the task Recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,9 +3,9 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,10 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  calculateDepth(arr) {
+    let counter = 1;
+		let currentDeep = 0;
+    arr.forEach((item, i) => {
+      if (arr[0] === undefined) return 1;
+      else if (Array.isArray(item)) {
+        currentDeep = Math.max(currentDeep, this.calculateDepth(item));
+      }
+    });
+
+		return currentDeep+counter;
+	}
 }
 
 module.exports = {
